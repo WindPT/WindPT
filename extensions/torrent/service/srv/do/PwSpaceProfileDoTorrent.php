7e34d22402de4853121fb60d243e17b21ad3b6f2
7IN0SAN9 <me@7in0.me>
2014-12-18 23:50:25 +0800

    Use Dao instead of direct sql

--- extensions/torrent/service/srv/do/PwSpaceProfileDoTorrent.php
+++ extensions/torrent/service/srv/do/PwSpaceProfileDoTorrent.php
@@ -1,5 +1,6 @@
 <?php
 defined('WEKIT_VERSION') or exit(403);
+
 /**
  * 空间资料页面
  *
@@ -7,42 +8,39 @@ defined('WEKIT_VERSION') or exit(403);
  * @copyright http://7in0.me
  * @license http://7in0.me
  */
-class PwSpaceProfileDoTorrent {
-	
-	/**
-	 * @param array $space
-	 */
-	public function app_TorrentDo($space) {
-		$config = require(realpath(dirname(__FILE__)).'/../../../../../conf/database.php');
-    $dbHandle = new PDO ( $config['dsn'], $config['user'], $config['pwd'] );
-    $dbHandle->setAttribute ( PDO::ATTR_EMULATE_PREPARES, false );
-    
-    $sql = 'SELECT SUM(downloaded) AS downloaded_total, SUM(uploaded) AS uploaded_total FROM pw_app_torrent_history WHERE uid = :id';
-    $sth = @$dbHandle->prepare ( $sql );
-    @$sth->execute ( array(':id'=>$space->{'spaceUid'}) );
-    $result = @$sth->fetch ( PDO::FETCH_ASSOC );
-    if (is_array ( $result )) {
-      $downloaded_total = floor($result ['downloaded_total']/1048567);
-      $uploaded_total = floor($result ['uploaded_total']/1048567);
+class PwSpaceProfileDoTorrent
+{
 
-      if ($downloaded_total != 0)
-        $rotio = round($result ['uploaded_total']/$result ['downloaded_total'], 2);
-      else
-        $rotio = 'Inf.';
+    /**
+     * @param array $space
+     */
+    public function appDo($space) {
+        $user = Wekit::load('EXT:torrent.service.dao.PwTorrentUserDao')->getTorrentUserByUid($space->{'spaceUid'});
+        $torrents = Wekit::load('EXT:torrent.service.dao.PwTorrentDao')->fetchTorrentByUid($space->{'spaceUid'});
+        $histories = Wekit::load('EXT:torrent.service.dao.PwTorrentHistoryDao')->fetchTorrentHistoryByUid($space->{'spaceUid'});
+        
+        $passkey = $user['passkey'];
+        
+        $posted = count($torrents);
+
+        foreach ($histories as $history) {
+            $downloaded_total+= $history['downloaded'];
+            $uploaded_total+= $history['uploaded'];
+        }
+        
+        $downloaded_total = floor($downloaded_total / 1048567);
+        $uploaded_total = floor($uploaded_total / 1048567);
+        
+        if ($downloaded_total != 0) $rotio = round($uploaded_total / $downloaded_total, 2);
+        else $rotio = 'Inf.';
+        
+        echo '<div class="space_profile"><h3><strong>PT个人信息</strong></h3>';
+        if ($space->{'visitUid'} == $space->{'spaceUid'}) echo '<dl class="cc"><dt>Passkey：</dt><dd><span id="passkey" style="background-color:rgb(51,51,51); color:rgb(51,51,51);">' . $passkey . '</span>&nbsp;<button id="btnToggle" onclick="if ($(\'#btnToggle\').text() == \'显示\') {$(\'#passkey\').css(\'background\', \'white\'); $(\'#btnToggle\').text(\'隐藏\');} else {$(\'#passkey\').css(\'background\', \'rgb(51,51,51)\');$(\'#btnToggle\').text(\'显示\');}">显示</button></dd></dl>';
+        echo '<dl class="cc"><dt>下载：</dt><dd>' . $downloaded_total . ' M</dd></dl>';
+        echo '<dl class="cc"><dt>上传：</dt><dd>' . $uploaded_total . ' M</dd></dl>';
+        echo '<dl class="cc"><dt>分享率：</dt><dd>' . $rotio . '</dd></dl>';
+        echo '<dl class="cc"><dt>发布：</dt><dd>' . $posted . '</dd></dl>';
+        echo '</div>';
     }
-    $sql = 'SELECT passkey from pw_app_torrent_user WHERE uid = :id';
-    $sth = @$dbHandle->prepare ( $sql );
-    @$sth->execute ( array(':id'=>$space->{'spaceUid'}) );
-    $result = @$sth->fetch ( PDO::FETCH_ASSOC );
-    $passkey = $result['passkey'];
-		echo '<div class="space_profile"><h3><strong>PT个人信息</strong></h3>';
-    if ($space->{'visitUid'} == $space->{'spaceUid'})
-      echo '<dl class="cc"><dt>Passkey：</dt><dd><span id="passkey" style="background-color:rgb(51,51,51); color:rgb(51,51,51);">'.$passkey.'</span><button id="btnToggle" onclick="if ($(\'#btnToggle\').text() == \'显示\') {$(\'#passkey\').css(\'background\', \'white\'); $(\'#btnToggle\').text(\'隐藏\');} else {$(\'#passkey\').css(\'background\', \'rgb(51,51,51)\');$(\'#btnToggle\').text(\'显示\');}">显示</button><button>重设</button></dd></dl>';
-    echo '<dl class="cc"><dt>下载：</dt><dd>'.$downloaded_total.' M</dd></dl>';
-    echo '<dl class="cc"><dt>上传：</dt><dd>'.$uploaded_total.' M</dd></dl>';
-    echo '<dl class="cc"><dt>分享率：</dt><dd>'.$rotio.'</dd></dl>';
-    echo '</div>';
-	}
 }
-
 ?>
