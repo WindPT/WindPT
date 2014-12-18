37af9f1d68a4b9b0f2b9f7aeb1eb7cb9e83aae46
7IN0SAN9 <me@7in0.me>
2014-12-18 23:48:55 +0800

    Add PwTorrentHistory Dao and Dm

---
+++ extensions/torrent/service/dao/PwTorrentHistoryDao.php
@@ -0,0 +1,29 @@
+<?php
+defined('WEKIT_VERSION') || exit('Forbidden');
+class PwTorrentHistoryDao extends PwBaseDao {
+    protected $_table = 'app_torrent_history';
+    protected $_pk = 'id';
+    protected $_dataStruct = array('id', 'uid', 'torrent', 'uploaded', 'uploaded_last', 'downloaded', 'downloaded_last', 'status');
+    public function getTorrentHistory($id) {
+        return $this->_get($id);
+    }
+    public function fetchTorrentHistoryByTorrent($torrent) {
+        $sql = $this->_bindTable("SELECT * FROM %s WHERE torrent = ?");
+        $smt = $this->getConnection()->createStatement($sql);
+        return $smt->queryAll(array($torrent));
+    }
+    public function fetchTorrentHistoryByUid($uid) {
+        $sql = $this->_bindTable("SELECT * FROM %s WHERE uid = ?");
+        $smt = $this->getConnection()->createStatement($sql);
+        return $smt->queryAll(array($uid));
+    }
+    public function addTorrentHistory($fields) {
+        return $this->_add($fields);
+    }
+    public function updateTorrentHistory($id, $fields, $increaseFields = array()) {
+        return $this->_update($id, $fields);
+    }
+    public function deleteTorrentHistory($id) {
+        return $this->_delete($id);
+    }
+}
