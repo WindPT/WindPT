a4450912281a6f6244883cdec850aa76b2102f73
7IN0SAN9 <me@7in0.me>
2015-10-29 21:43:09 +0800

    Show `Manage subscriptions` on space nav only in logged-in user's space

--- extensions/torrent/service/srv/do/PwSpaceNavDoTorrent.php
+++ extensions/torrent/service/srv/do/PwSpaceNavDoTorrent.php
@@ -6,6 +6,8 @@ class PwSpaceNavDoTorrent
 {
     public function appDo($space, $src)
     {
-        echo '<li><a href="' . WindUrlHelper::createUrl('/app/torrent/index/my') . '">种子订阅</a></li>';
+        if ($space->{'visitUid'} == $space->{'spaceUid'}) {
+            echo '<li><a href="' . WindUrlHelper::createUrl('/app/torrent/index/my') . '">种子订阅</a></li>';
+        }
     }
 }
