bf02a1fcf9807f70e9270202812c1c6e740f07e1
7IN0SAN9 <me@7in0.me>
2015-11-06 09:48:16 +0800

    rotio on header info

--- extensions/torrent/service/srv/do/PwHeaderInfoDoTorrent.php
+++ extensions/torrent/service/srv/do/PwHeaderInfoDoTorrent.php
@@ -16,6 +16,7 @@ class PwHeaderInfoDoTorrent
 
         if ($user) {
             $peers = Wekit::load('EXT:torrent.service.PwTorrentPeer')->fetchTorrentPeerByUid($user->uid);
+            $histories = Wekit::load('EXT:torrent.service.PwTorrentHistory')->fetchTorrentHistoryByUid($user->uid);
 
             $seeding = $leeching = 0;
             if (is_array($peers)) {
@@ -28,7 +29,20 @@ class PwHeaderInfoDoTorrent
                 }
             }
 
-            echo '<a href="' . WindUrlHelper::createUrl('space/profile/run?uid=' . $user->uid) . '">下载：' . $leeching . ' 做种：' . $seeding . '</a>';
+            if (is_array($histories)) {
+                foreach ($histories as $history) {
+                    $downloaded_total += $history['downloaded'];
+                    $uploaded_total += $history['uploaded'];
+                }
+            }
+
+            if ($downloaded_total != 0) {
+                $rotio = round($uploaded_total / $downloaded_total, 2);
+            } else {
+                $rotio = 'Inf.';
+            }
+
+            echo '<a href="' . WindUrlHelper::createUrl('space/profile/run?uid=' . $user->uid) . '">下载：' . $leeching . ' 做种：' . $seeding . ' 分享率：' . $rotio . '</a>';
         }
     }
 }
