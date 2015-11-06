883e8af7db544489b5d39f72444ee1ac193177eb
7IN0SAN9 <me@7in0.me>
2015-11-06 08:47:09 +0800

    Show tracker histories

--- extensions/torrent/service/srv/do/PwSpaceProfileDoTorrent.php
+++ extensions/torrent/service/srv/do/PwSpaceProfileDoTorrent.php
@@ -8,6 +8,7 @@ class PwSpaceProfileDoTorrent
 {
     public function appDo($space)
     {
+        $PwThread = Wekit::load('forum.PwThread');
         $showuserinfo = Wekit::C('site', 'app.torrent.showuserinfo');
 
         if (is_array($showuserinfo) && !in_array('profile', $showuserinfo)) {
@@ -15,17 +16,27 @@ class PwSpaceProfileDoTorrent
         }
 
         $user = Wekit::load('EXT:torrent.service.PwTorrentUser')->getTorrentUserByUid($space->spaceUid);
-        $histories = Wekit::load('EXT:torrent.service.PwTorrentHistory')->fetchTorrentHistoryByUid($space->spaceUid);
 
-        $this->torrents = Wekit::load('forum.PwThread')->getThreadByUid($space->spaceUid);
+        $this->histories = Wekit::load('EXT:torrent.service.PwTorrentHistory')->fetchTorrentHistoryByUid($space->spaceUid);
+        $this->torrents = $PwThread->getThreadByUid($space->spaceUid);
 
 
         $this->passkey = $user['passkey'];
 
-        if (is_array($histories)) {
-            foreach ($histories as $history) {
+        if (is_array($this->histories)) {
+            $PwTorrent = Wekit::load('EXT:torrent.service.PwTorrent');
+            foreach ($this->histories as $key => $history) {
                 $downloaded_total += $history['downloaded'];
                 $uploaded_total += $history['uploaded'];
+
+                $torrent = $PwTorrent->getTorrent($history['torrent']);
+                $thread = $PwThread->getThread($torrent['tid']);
+
+                if ($thread) {
+                    $this->histories[$key]['subject'] = $thread['subject'];
+                } else {
+                    unset($this->histories[$key]);
+                }
             }
         }
 
