6dd265b7f4d8e64830251af513850ba364c864ce
7IN0SAN9 <me@7in0.me>
2015-11-06 09:13:05 +0800

    Show tracker info on header_info

---
+++ extensions/torrent/service/srv/do/PwHeaderInfoDoTorrent.php
@@ -0,0 +1,34 @@
+<?php
+
+defined('WEKIT_VERSION') || exit('Forbidden');
+
+class PwHeaderInfoDoTorrent
+{
+    public function appDo()
+    {
+        $showuserinfo = Wekit::C('site', 'app.torrent.showuserinfo');
+
+        if (is_array($showuserinfo) && !in_array('headerinfo', $showuserinfo)) {
+            return;
+        }
+
+        $user = Wekit::getLoginUser();
+
+        if ($user) {
+            $peers = Wekit::load('EXT:torrent.service.PwTorrentPeer')->fetchTorrentPeerByUid($user->uid);
+
+            $seeding = $leeching = 0;
+            if (is_array($peers)) {
+                foreach ($peers as $peer) {
+                    if ($peer['seeder'] == 'yes') {
+                        $seeding++;
+                    } else {
+                        $leeching++;
+                    }
+                }
+            }
+
+            echo '<a>下载：' . $leeching . ' 做种：' . $seeding . '</a>';
+        }
+    }
+}
